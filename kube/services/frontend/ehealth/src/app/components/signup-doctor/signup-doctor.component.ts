2c79089c95084ff7ea406ddeecceab0355df1d3e
Arun Balchandran <arunbalachandran@users.noreply.github.com>
2023-05-26 04:59:25 -0500

    FEATURE-52: Added Sign up components for doctor & patient (#51)

---
+++ kube/services/frontend/ehealth/src/app/components/signup-doctor/signup-doctor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-doctor',
+  templateUrl: './signup-doctor.component.html',
+  styleUrls: ['./signup-doctor.component.css']
+})
+export class SignupDoctorComponent {
+
+}
